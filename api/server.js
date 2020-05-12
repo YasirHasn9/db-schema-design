04ffc09836afeff53c12f4490cf2321855ad1651
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-12 13:25:16 -0700

    build cars router

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const helmet = require("helmet");
 const server = express();
 
+const carsRouter = require("../cars/cars-router");
+
 server.use(express.json());
 server.use(helmet());
 
-server.use((req, res) => {
+server.use("/api/cars", carsRouter);
+server.use("/", (req, res) => {
   res.json({ message: "Welcome to db schema" });
 });
 module.exports = server;
---
+++ cars/cars-router.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const db = require("../data/dbConfig");
+
+router.get("/", async (req, res, next) => {
+
+});
+
+module.exports = router;
