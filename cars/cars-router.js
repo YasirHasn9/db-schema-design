4b6f9fd2fa6ae83e3190011f569f9df02d64b537
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-12 13:27:30 -0700

    get cars

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -3,7 +3,12 @@ const router = require("express").Router();
 const db = require("../data/dbConfig");
 
 router.get("/", async (req, res, next) => {
-
+  try {
+    const cars = await db("cars").select();
+    res.status(200).json(cars);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
